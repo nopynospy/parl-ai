645f30df0f918a5999b71961f69bf81082273313
nopynospy <nopynospy@gmail.com>
2021-09-16 17:08:52 +0800

    Update read jsonl

--- read-jsonl.py
+++ read-jsonl.py
@@ -3,7 +3,10 @@ import json
 with open('safety-human.jsonl', 'r') as json_file:
     json_list = list(json_file)
 
-for json_str in json_list:
-    result = json.loads(json_str)
-    print(f"result: {result}")
-    print(isinstance(result, dict))
+for i, entry in enumerate(json_list):
+    dialog = json.loads(entry)['dialog']
+    tweet = dialog[0][0]['text']
+    pred = dialog[0][1]['text']
+    print('\nTweet' + str(i))
+    print(tweet)
+    print(pred)
