87c252b947999a328dda8cbe0cc1ee198ffbf99c
nopynospy <nopynospy@gmail.com>
2021-09-16 10:26:12 +0800

    Add read jsonl

---
+++ read-jsonl.py
@@ -0,0 +1,9 @@
+import json
+
+with open('safety-human.jsonl', 'r') as json_file:
+    json_list = list(json_file)
+
+for json_str in json_list:
+    result = json.loads(json_str)
+    print(f"result: {result}")
+    print(isinstance(result, dict))
