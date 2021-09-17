a8d47f3e2d194ea40e73e641d0ec7ed4f8b1b91e
nopynospy <nopynospy@gmail.com>
2021-09-17 11:25:03 +0800

    Add spellchecker and contractions

---
+++ spell-checker.py
@@ -0,0 +1,23 @@
+import re
+from spellchecker import SpellChecker
+
+spell = SpellChecker()
+
+with open('twitter.txt', 'r', encoding="utf8") as fp:
+    Lines = fp.readlines()
+    for line in Lines:
+        misspelled = spell.unknown(re.compile('\w+').findall(line))
+        for word in misspelled:
+            print('\n' + line)
+            print(word)
+            print(spell.correction(word))
+
+# # find those words that may be misspelled
+# misspelled = spell.unknown(['analytica', 'sqn', 'cve', 'rss', 'lbds', 'scotus'])
+
+# for word in misspelled:
+#     # Get the one `most likely` answer
+#     print(spell.correction(word))
+
+#     # Get a list of `likely` options
+#     print(spell.candidates(word))
